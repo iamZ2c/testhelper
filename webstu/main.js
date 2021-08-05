ffcc530598b8fdbac5d083cebfda939de0ed398d
iam2cc <1059995908@qq.com>
2021-08-05 20:50:13 +0800

    456

--- webstu/main.js
+++ webstu/main.js
@@ -1,6 +1,13 @@
 const b = new Vue({
     el: '#app',
     data: {
+        xxyy:[],
+        fruit:'',
+        fruits:[],
+        fruitxx:['卧槽','你草','大家草'],
+        hobbies:[],
+        is_agree:false,
+        what_fuck:'男',
         message: "z",
         books: [{
             id: 1,
