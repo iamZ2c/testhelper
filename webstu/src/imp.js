a6c509ef6452f533c102d92c2d2777704d9f8d1a
iam2cc <1059995908@qq.com>
2021-08-20 10:43:34 +0800

    update webpack.config.json

--- webstu/src/imp.js
+++ webstu/src/imp.js
@@ -1,3 +1,14 @@
 require('../css/normal.css')
 import {b} from './main.js'
 console.log(b)
+
+import Vue from "vue";
+
+
+
+new Vue({
+    el: '#app',
+    data:{
+        message: "hello webpack"
+    }
+})
--- webstu/webpack.config.js
+++ webstu/webpack.config.js
@@ -4,10 +4,48 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bunde.js',
+        // 解决file-loader，打包打在dist文件里，无法加载路径的问题。
+        publicPath: 'dist/',
     },
+    resolve:{
+        // alias:别名,給需要使用的vue文件取个别名。
+        alias:{
+            'vue$': 'vue/dist/vue.esm.js'
+        }
+    },
+    // css-loader负责把css文件进行加载
+    // style-loader负责把样式添加dom里面
+    // loader是从右往左读取的
     module: {
         rules: [
-            {test: /\.css$/, use: ['style-loader', 'css-loader']},
+            {
+                test: /\.css$/,
+                use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(png|jpg|gif)$/i,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        // 家在的图片小于limit会将图片编译成base64字符串形式，如果是大于了limit就会使用file-loader来进行加载
+                        options: {
+                            limit: 400,
+                            name: 'img/[name].[hash:8].[ext]'
+                        },
+                    },
+                ],
+            },
+            // es6 转化es5语法loader配置
+            {
+                test: /\.m?js$/,
+                exclude: /(node_modules|bower_components)/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['es2015']
+                    }
+                }
+            },
         ],
     },
 }
