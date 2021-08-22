dcbacb47fb1b082cabc28ad286350717155a8fd9
iam2cc <1059995908@qq.com>
2021-08-22 16:52:23 +0800

    plugin

--- webstu/src/imp.js
+++ webstu/src/imp.js
@@ -4,9 +4,10 @@ import {b} from './main.js'
 console.log(b)
 
 import Vue from "vue";
-import App from "./vue/App"
+// import App from "./vue/App"
+import App from "./vue/App.vue";
 //拆分出来
-
+// npm install --save-dev vue-loader vue-template-compiler
 
 
 new Vue({
---
+++ webstu/src/vue/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <span class="span">我是span</span>
+    <button @click="cil()">按钮</button>{{message}}
+  </div>
+</template>
+
+<script>
+export default {
+  name: "App",
+  data() {
+    return {
+      message: "hello webpack"
+    }
+  },
+  methods: {
+    cil() {
+      this.message = 'hello web'
+    },
+  }
+}
+</script>
+
+<style scoped>
+  .span {
+    font-size: 30px;
+  }
+</style>
