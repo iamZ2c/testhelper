aff8b388a8481b1ef81de9097235a1bb7a5634fe
iam2cc <1059995908@qq.com>
2021-09-05 19:04:38 +0800

    v-slot

--- superwardrobe/src/components/content/MainTabBar.vue
+++ superwardrobe/src/components/content/MainTabBar.vue
@@ -11,22 +11,39 @@
         <template v-slot:bar-text>
           <div>首页</div>
         </template>
-
       </tab-bar-item>
       <tab-bar-item path="/cart">
-        <img slot="bar-icon" src="../../assets/img/footer02.png" alt="">
-        <img slot="bar-icon-active" src="../../assets/img/footer002.png" alt="">
-        <div slot="bar-text">分类</div>
+        <template v-slot:bar-icon>
+          <img src="../../assets/img/footer02.png" alt="">
+        </template>
+        <template v-slot:bar-icon-active>
+          <img src="../../assets/img/footer002.png" alt="">
+        </template>
+        <template v-slot:bar-text>
+          <div>分类</div>
+        </template>
       </tab-bar-item>
       <tab-bar-item path="/category">
-        <img slot="bar-icon" src="../../assets/img/footer03.png" alt="">
-        <img slot="bar-icon-active" src="../../assets/img/footer003.png" alt="">
-        <div slot="bar-text">购物车</div>
+        <template v-slot:bar-icon>
+          <img src="../../assets/img/footer03.png" alt="">
+        </template>
+        <template v-slot:bar-icon-active>
+          <img src="../../assets/img/footer003.png" alt="">
+        </template>
+        <template v-slot:bar-text>
+          <div>购物车</div>
+        </template>
       </tab-bar-item>
       <tab-bar-item path="/profile">
-        <img slot="bar-icon" src="../../assets/img/footer04.png" alt="">
-        <img slot="bar-icon-active" src="../../assets/img/footer004.png" alt="">
-        <div slot="bar-text">我的</div>
+        <template v-slot:bar-icon>
+          <img src="../../assets/img/footer04.png" alt="">
+        </template>
+        <template v-slot:bar-icon-active>
+          <img src="../../assets/img/footer004.png" alt="">
+        </template>
+        <template v-slot:bar-text>
+          <div>我的</div>
+        </template>
       </tab-bar-item>
     </tab-bar>
     <router-view></router-view>
