6b8b23a74a6b2cd85b4f4bc3dd1e96c4abbc1c3f
iam2cc <1059995908@qq.com>
2021-11-08 11:07:34 +0800

    asd

--- superwardrobe/src/views/detail/detailcpn/DetailImageInfo.vue
+++ superwardrobe/src/views/detail/detailcpn/DetailImageInfo.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="detail-info" v-if="Object.keys(detailInfo).length !== 0">
-    {{detailInfo.desc}}
+    <div style="width: 8px;height: 8px; background-color: black; display: inline-block; margin-left: 10px"></div>
+    <div style="width: calc(100% - 20px); height: 1px; background-color: black; display: inline-block; position: absolute;margin-top: 7px"></div>
+    <div style="margin: 10px 10px">{{detailInfo.desc}}</div>
+    <div style="width: calc(100% - 20px); height: 1px; background-color: black; display: inline-block;"></div>
+    <div style="width: 8px;height: 8px; background-color: black; display: inline-block; margin-right: 10px"></div>
+
     <div class="detail-img" v-for="(url, index) in detailInfo.detailImage[0].list" :key="index">
       <img :src="url" alt="" @load="detailLoadImg">
     </div>
@@ -30,5 +35,8 @@ export default {
 <style scoped>
 .detail-img > img{
   width: calc(100vw);
+}
+.detail-img{
+  margin-top: 10px;
 }
 </style>
